7d8efc822c51a14e9e0126db64004d832971db6a
JimmyMD20 <j.mujicad@uniandes.edu.co>
2026-08-20 15:21:32 -0500

    Completar funciones de array_list y avance de single_linked_list

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -5,3 +5,56 @@ def new_list():
         "size": 0,
     }
     return new_list
+
+def get_element(my_list, pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos:
+        node = node["next"]
+        searchpos += 1
+    return node["info"]
+
+def is_present(my_list, element, cmp_function):
+    is_in_array = False
+    temp = my_list["first"]
+    count = 0
+    while not is_in_array and temp is not None:
+        if cmp_function(element, temp["info"]) == 0:
+            is_in_array = True
+        else:
+            temp = temp["next"]
+            count += 1
+    
+    if not is_in_array:
+        count = -1
+    return count
+
+def is_empty(my_list):
+    return my_list["size"] == 0
+
+def add_first(my_list, element):
+    my_list["first"] = {"info": element, "next": my_list["first"]}
+    if my_list["size"] == 0:
+        my_list["last"] = my_list["first"]
+    my_list["size"] += 1
+    return my_list
+
+def add_last(my_list, element):
+    new_node = {"info": element, "next": None}
+    if my_list["size"] == 0:
+        my_list["first"] = new_node
+    else:
+        my_list["last"]["next"] = new_node
+    my_list["last"] = new_node
+    my_list["size"] += 1
+    return my_list
+
+def size(my_list):
+    return my_list["size"]
+
+def first_element(my_list):
+    if is_empty(my_list):
+        raise Exception('IndexError: list index out of range')
+    else:
+        return my_list["first"]["info"]
+
