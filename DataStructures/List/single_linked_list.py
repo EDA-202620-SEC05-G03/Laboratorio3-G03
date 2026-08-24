2114bd028bedf8e136b89d8b63bb829f3874c20d
SamuelDiaz1010 <se.diaza12@uniandes.edu.co>
2026-08-24 17:09:02 -0500

    cambios para funciones: exchange, change_info, sub

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -126,3 +126,38 @@ def delete_element(my_list, pos):
             my_list["size"] -= 1
         return my_list
     
+def change_info (my_list, pos, new_info):
+    if pos < 0 or pos >= size(my_list):
+        raise IndexError('list index out of range')
+    else:
+        node = my_list["first"]
+        for i in range(pos):
+            node = node["next"]
+        node["info"] = new_info
+    return my_list
+
+def exchange(my_list, pos_1, pos_2):
+    if pos_1 < 0 or pos_1 >= size(my_list) or pos_2 < 0 or pos_2 >= size(my_list):
+        raise IndexError('list index out of range')
+    else:
+        node_1 = my_list["first"]
+        for i in range(pos_1):
+            node_1 = node_1["next"]
+        node_2 = my_list["first"]
+        for i in range(pos_2):
+            node_2 = node_2["next"]
+        node_1["info"], node_2["info"] = node_2["info"], node_1["info"]
+    return my_list
+
+def sub_list(my_list, pos_i, num_elements):
+    if pos_i < 0 or pos_i >= size(my_list) or num_elements < 0 or pos_i + num_elements > size(my_list):
+        raise IndexError('list index out of range')
+    else:
+        nueva_lista = new_list()
+        node = my_list["first"]
+        for i in range(pos_i):
+            node = node["next"]
+        for i in range(num_elements):
+            nueva_lista = add_last(nueva_lista, node["info"])
+            node = node["next"]
+    return nueva_lista
