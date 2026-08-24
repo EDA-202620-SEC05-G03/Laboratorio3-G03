7a2282dcba0b940d2b3e2cc9fae43c600a48f7df
JimmyMD20 <j.mujicad@uniandes.edu.co>
2026-08-24 13:05:55 -0500

    Algunas funciones del singled_linked_list

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -58,3 +58,71 @@ def first_element(my_list):
     else:
         return my_list["first"]["info"]
 
+def last_element(my_list):
+    if is_empty(my_list):
+        raise Exception('IndexError: list index out of range')
+    else:
+        return my_list["last"]["info"]
+    
+def remove_first(my_list):
+    if is_empty(my_list):
+        raise Exception('IndexError: list index out of range')
+    else:
+        elem = my_list["first"]["info"]
+        my_list["first"] = my_list["first"]["next"]
+        my_list["size"] -= 1
+        if my_list["size"] == 0:
+            my_list["last"] = None
+        return elem
+    
+def remove_last(my_list):
+    if is_empty(my_list):
+        raise Exception('IndexError: list index out of range')
+    else:
+        elem = my_list["last"]["info"]
+        if my_list["size"] == 1:
+            my_list["first"] = None
+            my_list["last"] = None
+        else:
+            node = my_list["first"]
+            for i in range(my_list["size"]-2):
+                node = node["next"]
+            node["next"] = None
+            my_list["last"] = node
+        my_list["size"] -= 1
+    return elem
+
+def insert_element(my_list, element, pos):
+    if pos < 0 or pos > size(my_list):
+        raise Exception('IndexError: list index out of range')
+    else:
+        if pos == 0:
+            my_list = add_first(my_list, element)
+        elif pos == size(my_list):
+           my_list = add_last(my_list, element)
+        else:
+            new_node = {'info': element, 'next': None}
+            node = my_list["first"]
+            for i in range(pos-1):
+                node = node["next"]
+            new_node["next"] = node["next"]
+            node["next"] = new_node
+            my_list["size"] += 1
+    return my_list
+
+def delete_element(my_list, pos):
+    if pos < 0 or pos >= size(my_list):
+        raise Exception('IndexError: list index out of range')
+    else:
+        if pos == 0:
+            remove_first(my_list)
+        elif pos == size(my_list) - 1:
+            remove_last(my_list)
+        else:
+            actual = my_list["first"]
+            for i in range(1, pos):
+                actual = actual["next"]
+            actual["next"] = actual["next"]["next"]
+            my_list["size"] -= 1
+        return my_list
+    
